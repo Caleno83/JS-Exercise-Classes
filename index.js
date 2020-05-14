faa253fa670d89296c25e1a033d7842d1949adee
Jorge Jimenez <calenocam24@gmail.com>
2020-05-13 20:34:54 -0400

    finished task 4

--- index.js
+++ index.js
@@ -125,16 +125,16 @@ class Car {
 */
 class Lambdasian {
 
-  constructor(obj) {
-    this.name = obj.name;
-    this.age = obj.age;
-    this.location = obj.location;
+  constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
   };
 
     speak() {
       return `Hello my name is ${this.name}, I am from ${this.location}.`
-    }
-}
+    };
+};
 
 /*
   TASK 4
@@ -150,11 +150,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor (attr){
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
 
-  
+  demo(subject) {
+ return `Today we are learning about ${subject}`;
+  };
 
-}
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`;
+  };
+};
 
 /*
   TASK 5
